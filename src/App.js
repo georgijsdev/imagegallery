aba192840ccb2489004c1df49850eb965c7d2b5b
Georgijs Ventura <ventura55g@gmail.com>
2020-10-09 22:41:51 +0100

    images error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-
+import ImageCard from './components/ImageCard';
 
 function App() {
   const {images, setImages} = useState([]);
@@ -13,46 +13,22 @@ function App() {
       .then(res => res.json())
       .then(data => {
         setImages(data.hits);
-        setIsLoading(false); // 22:00
+        setIsLoading(false);
       })
       .catch(err => console.log(err))
-  })
+  }, []);
 
   return (
-    <div>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
-        <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
-            Photo by Georgijs Dev
-          </div>
-          <ul>
-            <li>
-              <strong>Views: </strong>
-              4000
-            </li>
-            <li>
-              <strong>Downloads: </strong>
-              158
-            </li>
-            <li>
-              <strong>Likes: </strong>
-              267
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
-      </div>
+    <div className="container mx-auto">
+      {/* <ImageSearch searchText={(text) => setTerm(text)} /> */}
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
